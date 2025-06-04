62e91c59c9c0f7278e5c7370b8246594bd24e7e6
shirkenitin347 <shirkenitin347@gmail.com>
2025-06-04 18:13:31 +0530

    terraform workflow

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-rearc-terraform-state"
+    bucket = "dev-rearc-terraform"
     key    = "rearc/terraform.tfstate"
     region = "ap-south-1"
     # dynamodb_table = "terraform-locks"   # Optional but recommended
