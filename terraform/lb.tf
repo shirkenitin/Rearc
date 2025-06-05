dcd419f3fdef48a895f20dd4f3a5f225bb3d50d4
shirkenitin347 <shirkenitin347@gmail.com>
2025-06-05 11:21:33 +0530

    terraform

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -56,7 +56,7 @@ resource "aws_lb_target_group" "quest_lb_tg" {
 
 resource "aws_lb_listener" "quest_lb_listener" {
   load_balancer_arn = aws_lb.quest_lb.arn
-  port              = 3000
+  port              = 80
   protocol          = "HTTP"
   default_action {
     type             = "forward"
