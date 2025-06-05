9d8f5ba58696bba14eaea50081fc0d9730ba4c5a
shirkenitin347 <shirkenitin347@gmail.com>
2025-06-05 12:30:58 +0530

    terraform

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "lb" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
   }
