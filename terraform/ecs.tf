f0e874ba25e0f0a0d8d68033d8bcb36ccf66c1e7
shirkenitin347 <shirkenitin347@gmail.com>
2025-06-05 10:16:39 +0530

    terraform

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -31,8 +31,8 @@ resource "aws_ecs_task_definition" "quest" {
   family                   = "${var.environment}-${var.project_name}-backend"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "1024"
+  memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([
